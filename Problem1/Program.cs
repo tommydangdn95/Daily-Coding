b52464cdce461ce5023eb1ecc2d0654015974f94
Thuy Dang Tran <thuy.dang@getz.co>
2020-03-25 16:03:54 +0700

    add problem day 1 and day 2

---
+++ Problem1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Problem1
+{
+
+    /** Problem
+     * Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
+     *For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] lists = new int[] { 10, 15, 3, 17, 14 };
+            var isHasPair = HasPairWithSum(lists, 17);
+            if (isHasPair)
+            {
+                Console.WriteLine("Has Pair");
+            }
+            else
+            {
+                Console.WriteLine("Doesn't");
+            }
+
+        }
+
+        public static bool HasPairWithSum(int[] pairSum, int sum)
+        {
+            foreach(var item in pairSum)
+            {
+                var remain = sum - item;
+                if (pairSum.Contains(remain))
+                    return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ Problem2/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace Problem2
+{
+    class Program
+    {
+        /** Problem
+         * Given an array of integers, return a new array such that each element at index i of the new array is the product of all the numbers in the original array except the one at i.
+         * For example, if our input was [1, 2, 3, 4, 5], the expected output would be [120, 60, 40, 30, 24]. If our input was [3, 2, 1], the expected output would be [2, 3, 6]
+         * 
+         */
+        static void Main(string[] args)
+        {
+            int[] arrays = new int[] { 1, 2, 3, 4, 5 };
+            var newProductArrays = ProductArray(arrays);
+            Console.Write("[ ");
+            foreach(var item in newProductArrays)
+            {
+                Console.Write(item + " ");
+            }
+            Console.Write(" ]");
+        }
+
+        private static int[] ProductArray(int[] arrays)
+        {
+            int[] newArrays = new int[arrays.Length];
+            var countNumberZero = 0;
+            for(int i=0;i<arrays.Length; i++)
+            {
+                if(arrays[i] ==0)
+                {
+                    countNumberZero++;
+                }
+            }
+
+            int products = 1;
+            switch (countNumberZero)
+            {
+                case 0:
+                    for (int i = 0; i < arrays.Length; i++)
+                    {
+                        products *= arrays[i];
+                    }
+
+                    for (int i = 0; i < arrays.Length; i++)
+                    {
+                        newArrays[i] = products / arrays[i];
+                    }
+                    break;
+                case 1:
+                    for(int i=0; i< arrays.Length; i++)
+                    {
+                        if(arrays[i] != 0)
+                        {
+                            products *= arrays[i];
+                        }
+                    }
+
+                    for (int i=0; i< arrays.Length; i++)
+                    {
+                        if(arrays[i] == 0)
+                        {
+                            newArrays[i] = products;
+                        }
+                    }
+                    break;
+                default:
+                    break;
+            }
+
+            return newArrays;
+        }
+    }
+}
